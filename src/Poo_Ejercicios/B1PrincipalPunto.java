813e2a671056cc25891eb6de8429435fead50b41
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2026-01-13 12:56:59 +0100

    B1 corregido

---
+++ src/Poo_Ejercicios/B1PrincipalPunto.java
@@ -0,0 +1,14 @@
+package Poo_Ejercicios;
+
+public class B1PrincipalPunto {
+
+	 public static void main(String[] args) {
+	        B1Punto p1 = new B1Punto(5, 0);
+	        B1Punto p2 = new B1Punto(10, 10);
+	        B1Punto p3 = new B1Punto(-3, 7);
+
+	        System.out.println("(" + p1.x + "," + p1.y + ")");
+	        System.out.println("(" + p2.x + "," + p2.y + ")");
+	        System.out.println("(" + p3.x + "," + p3.y + ")");
+	    }
+	}
---
+++ src/Poo_Ejercicios/B1Punto.java
@@ -0,0 +1,14 @@
+package Poo_Ejercicios;
+
+public class B1Punto {
+	
+	    int x;
+	    int y;
+
+	    public B1Punto(int x, int y) {
+	        this.x = x;
+	        this.y = y;
+	    }
+	}
+
+
